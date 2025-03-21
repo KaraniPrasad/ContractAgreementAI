f3f6b7bd89a5d94f3fbf46008bfc68765ccbb992
KaraniPrasad <prasad.karani@gmail.com>
2025-03-21 14:17:40 +0530

    seventh commit

--- src/ContractAgreement_ai/main.py
+++ src/ContractAgreement_ai/main.py
@@ -10,6 +10,12 @@ from src.ContractAgreement_ai.tools.Tools import ClauseLibraryLoader
 def main():
 
     # Initialize the LLM and Graph
+    api_key = os.getenv("GROQ_API_KEY")
+    if api_key is None:
+        raise ValueError("API key is not set. Please set the 'GROQ_API_KEY' environment variable.")
+    else:
+        print("API Key loaded successfully!")
+        
     llm = LLM(api_key=os.getenv("GROQ_API_KEY"))
     graph = Graph()
 
